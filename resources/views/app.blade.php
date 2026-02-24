73f003baf1de528af4f37dc924de7d6082a8e250
codeWithMe <codewithhassan.404.not.found@gmail.com>
2026-02-24 10:16:28 +0100

    fix bug

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -91,7 +91,7 @@
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-heading antialiased">
         @inertia
     </body>
 </html>
